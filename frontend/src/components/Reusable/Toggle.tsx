6c553c879d36819094f66ca9f660972251247cf1
Priyansi Parida <priyansiparida@Priyansis-MacBook-Air.local>
2026-05-12 11:48:38 +0530

    dark toggle update

--- frontend/src/components/Reusable/Toggle.tsx
+++ frontend/src/components/Reusable/Toggle.tsx
@@ -7,14 +7,13 @@ const Toggle = () => {
   return (
     <Tooltip>
       <button
-        className={`${isOpen ? "bg-blue-600" : "bg-gray-600"}  w-14 h-8 rounded-full transition-all duration-300 cursor-pointer shadow-md flex items-center`}
+        aria-label="Toggle dark mode"
+        className={`${isOpen ? "bg-blue-600" : "bg-gray-600"}  w-14 h-7 rounded-full transition-all duration-300 cursor-pointer shadow-md flex items-center hover:scale-105`}
         onClick={() => setOpen((prev) => !prev)}
       >
         <div
-          className={` w-6 h-6 rounded-full bg-white ${!isOpen ? "translate-x-0" : "translate-x-6"} transition-all duration-300`}
-        >
-          {isOpen ? "🌙" : "☀️"}
-        </div>
+          className={` w-5 h-5 rounded-full bg-white ${!isOpen ? "translate-x-0" : "translate-x-8"} transition-all duration-300 flex items-center justify-center`}
+        ></div>
       </button>
     </Tooltip>
   );
